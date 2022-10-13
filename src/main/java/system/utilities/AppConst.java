888218b19934a41b0bd02df4345f06e72ee68d97
EmilianoZerbino <103704056+EmilianoZerbino@users.noreply.github.com>
2022-10-13 14:33:20 -0300

    Update AppConst.java

--- src/main/java/system/utilities/AppConst.java
+++ src/main/java/system/utilities/AppConst.java
@@ -12,8 +12,8 @@ public class AppConst {
 	
 	//Default values to send emails through sendgrid 
 	
-	public static String defaultCompanyEmail = "somosfundacionmas2@gmail.com";
-	public static String SendGredApiKey = "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";
+	public static String defaultCompanyEmail = "xxxxxxxxxxxxxxxxxxxxxxxxx";
+	public static String SendGredApiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 	public static String emailSubjectText = "Bienvenido al mundo de Disney %s";
 	public static String emailContentText = "Bienvenido %s, estamos orgullosos de que sea parte de nosotros.";
 }
